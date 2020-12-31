3962fd65fb452660bcb47254470b78f8fa487056
ArraygenR <akshata@arraygen.co.in>
2020-12-31 10:35:44 +0530

    edit patient

--- PanelProject/app/models/patients.py
+++ PanelProject/app/models/patients.py
@@ -4,6 +4,7 @@ from .panels import Panels
 class Patients(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     date = db.Column(db.String(64), nullable=False)
+    patient_id = db.Column(db.String(20), nullable=False)
     f_name = db.Column(db.String(64), nullable=False)
     l_name = db.Column(db.String(64), nullable=False)
     gender = db.Column(db.String(20), nullable=False)
@@ -13,8 +14,20 @@ class Patients(db.Model):
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
     address = db.Column(db.Text(), nullable=False)
+    order_by = db.Column(db.String(20), nullable=False)
+    collection_date = db.Column(db.String(20), nullable=False)
+    received_date = db.Column(db.String(20), nullable=False)
+    report_generated = db.Column(db.String(20), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    bmi = db.Column(db.Integer, nullable=False)
+    allergic_to = db.Column(db.String(255), nullable=True)
+    additional1 = db.Column(db.String(255), nullable=True)
+    additional2 = db.Column(db.String(255), nullable=True)
+    additional3 = db.Column(db.String(255), nullable=True)
+    remark = db.Column(db.Text(), nullable=True)
     panels = db.relationship(Panels, secondary='patient_panels')
-
 
 class Patient_panels(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
@@ -27,3 +40,7 @@ class Patient_panels(db.Model):
        db.Integer,
        db.ForeignKey('panels.id'),
        primary_key = True)
+
+    dna_results = db.Column(db.String(255), nullable= True)
+    blood_results = db.Column(db.String(255), nullable= True)
+    allergy_results = db.Column(db.String(255), nullable= True)
